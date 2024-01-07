f12ad1fe596d1601fcaa93e04241d78e0642a0ed
Re1ncarnat10 <128154276+Re1ncarnat10@users.noreply.github.com>
2024-01-07 20:17:20 +0100

    Users models (#3)
    * user and projects
    
    * user and projects

--- Backend/database/migrations/2024_01_07_175527_create_projects_table.php
+++ Backend/database/migrations/2024_01_07_175527_create_projects_table.php
@@ -11,14 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('projects', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
             $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
-            $table->timestamp('expires_at')->nullable();
+            $table->dateTime('start_date');
+            $table->dateTime('end_date_planned');
+            $table->enum('status', ['new', 'ongoing', 'completed']);
+            $table->text('participants')->nullable();
+            $table->foreignId('owner_id')->constrained('users');
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
@@ -28,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('projects');
     }
 };
