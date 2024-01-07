6ca00d844c48080f3050009b6132dc653235d381
Re1ncarnat10 <128154276+Re1ncarnat10@users.noreply.github.com>
2024-01-07 21:58:35 +0100

    user and projects

--- Backend/app/Http/Controllers/ProjectController.php
+++ Backend/app/Http/Controllers/ProjectController.php
@@ -2,10 +2,40 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    public function index()
+    {
+        $projects = Project::all();
+        return response()->json($projects);
+    }
+
+    public function show($id)
+    {
+        $project = Project::findOrFail($id);
+        return response()->json($project);
+    }
+
+    public function store(Request $request)
+    {
+        $project = Project::create($request->all());
+        return response()->json($project, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $project = Project::findOrFail($id);
+        $project->update($request->all());
+        return response()->json($project, 200);
+    }
+
+    public function destroy($id)
+    {
+        $project = Project::findOrFail($id);
+        $project->delete();
+        return response()->json(null, 204);
+    }
 }
