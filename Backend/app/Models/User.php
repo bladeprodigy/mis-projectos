56950d6ed577242734cbb8e5cfcf1fe975bfc99c
Re1ncarnat10 <128154276+Re1ncarnat10@users.noreply.github.com>
2024-01-08 02:08:42 +0100

    user and projects (#8)

--- Backend/app/Models/User.php
+++ Backend/app/Models/User.php
@@ -11,7 +11,7 @@ class User extends Authenticatable
     use HasFactory, Notifiable;
 
     protected $fillable = [
-        'username', 'name', 'surname', 'email', 'password', 'phone',
+        'username', 'email', 'password',
     ];
 
     protected $hidden = [
