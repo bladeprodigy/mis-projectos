6aa9225d679d7e6440a936edcb41f0b51c83a108
PrezesG <129513239+PrezesG@users.noreply.github.com>
2024-01-10 01:08:43 +0100

    3
    3

--- Backend/app/Providers/AppServiceProvider.php
+++ Backend/app/Providers/AppServiceProvider.php
@@ -20,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if(App::environment('production')){
+            URL::forceShceme('https');
+        }
         Schema::defaultStringLength(191);
     }
 }
