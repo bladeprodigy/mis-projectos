231b50bb34d8e8531f4b37e6802aacc24c9af43a
bladeprodigy <a.galeza.552@studms.ug.edu.pl>
2024-01-10 16:19:40 +0100

    changing core to make calls from local env

--- Backend/config/cors.php
+++ Backend/config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'https://therewillbesomethingherelaterforsurexyz.com',
+    ],
 
     'allowed_origins_patterns' => [],
 
