6b2f9b666b4ca8f2bd79be366c0bc06bd43c3e4a
PrezesG <129513239+PrezesG@users.noreply.github.com>
2024-01-10 01:28:09 +0100

    4
    4

--- Backend/app/Providers/AppServiceProvider.php
+++ Backend/app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
-
+// use Illuminate\Support\Facades\App;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -20,9 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if(App::environment('production')){
-            URL::forceShceme('https');
-        }
+        // if(App::environment('production')){
+        //     URL::forceShceme('https');
+        // }
         Schema::defaultStringLength(191);
     }
 }
